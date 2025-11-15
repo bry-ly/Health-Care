b797c0d53598a657f3f4f8537ddd0f042c1f5145
Brylle <bryanpalay119@gmail.com>
2025-11-16 03:29:15 +0800

    Add landing page components to enhance user experience. Integrate Features, PatientContent, DoctorContent, and Footer sections into the main page layout for improved content presentation and navigation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import HeroSection from "@/components/landing/hero-section";
-
+import Features from "@/components/landing/features";
+import PatientContent from "@/components/landing/patient-content";
+import DoctorContent from "@/components/landing/doctor-content";
+import Footer from "@/components/landing/footer";
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <Features />
+      <PatientContent />
+      <DoctorContent />
+      <Footer />
     </main>
   );
 }
