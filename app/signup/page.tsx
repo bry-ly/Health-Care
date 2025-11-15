8f2b9c8b8a3394808acee06d5f9df1dc6f34e616
Brylle <bryanpalay119@gmail.com>
2025-11-15 22:39:31 +0800

    Refactor login and signup pages to use Link component for navigation and update branding to 'HealthCare'. Replace GalleryVerticalEnd icon with IconHeartbeat for a consistent design across authentication pages.

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,17 +1,20 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
+import Link from "next/link";
 import { SignupForm } from "@/components/auth/signup-form";
+import { IconHeartbeat } from "@tabler/icons-react";
 
 export default function SignupPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+    <div className="bg-muted flex lg:h-screen min-h-svh md:min-h-screen flex-col items-center justify-center gap-4 overflow-y-auto p-4 md:p-6">
+      <div className="flex w-full max-w-md flex-col gap-4">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <IconHeartbeat className="size-4" />
           </div>
-          Acme Inc.
-        </a>
+          Healthcare
+        </Link>
         <SignupForm />
       </div>
     </div>
