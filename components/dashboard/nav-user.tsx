cf089d2a6eaa36caf090c06d5b11427a57ebd7d1
Brylle <bryanpalay119@gmail.com>
2025-11-18 08:00:05 +0800

    Remove unused dropdown menu items from the user navigation component to streamline the interface and improve user experience.

--- components/dashboard/nav-user.tsx
+++ components/dashboard/nav-user.tsx
@@ -93,22 +93,6 @@ export function NavUser({
                 </div>
               </div>
             </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <IconUserCircle />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconCreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <IconNotification />
-                Notifications
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
             <DropdownMenuItem onClick={handleLogout}>
               <IconLogout />
               Log out
